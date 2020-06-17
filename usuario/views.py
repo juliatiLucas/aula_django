ec4db537c8b3a3a28c0cc732412d1a1846e911de
Lucas Juliati <vinnilucas4@gmail.com>
2020-06-17 20:49:26 -0300

    resolvido problema de login e cadastro

--- usuario/views.py
+++ usuario/views.py
@@ -5,27 +5,32 @@ from utils import serializer
 class ProfessorView(views.APIView):
     def post(self, request, **kwargs):
         data = request.data
-       
-        verify = Professor.objects.filter(email=data['email'], senha=data['senha'])
-        if (len(verify) == 0):
-            professor = Professor(nome=data['nome'], email=data['email'], senha=data['senha'])
-            professor.save()
-            return response.Response(serializer.professor_to_json(professor), status.HTTP_201_CREATED)
-        else: 
+        try:
+            verify = Professor.objects.filter(email=data['email'], senha=data['senha'])
+            if (len(verify) == 0):
+                professor = Professor(nome=data['nome'], email=data['email'], senha=data['senha'])
+                professor.save()
+                return response.Response(serializer.professor_to_json(professor), status.HTTP_201_CREATED)
+            else: 
+                return response.Response({}, status.HTTP_400_BAD_REQUEST)
+        except:
             return response.Response({}, status.HTTP_400_BAD_REQUEST)
     
 
 class AlunoView(views.APIView):
     def post(self, request, **kwargs):
         data = request.data
-       
-        verify = Aluno.objects.filter(email=data['email'], senha=data['senha'])
-        if (len(verify) == 0):
-            aluno = Aluno(nome=data['nome'], email=data['email'], senha=data['senha'])
-            aluno.save()
-            return response.Response(serializer.aluno_to_json(aluno), status.HTTP_201_CREATED)
-        else: 
+        try:
+            verify = Aluno.objects.filter(email=data['email'], senha=data['senha'])
+            if (len(verify) == 0):
+                aluno = Aluno(nome=data['nome'], email=data['email'], senha=data['senha'])
+                aluno.save()
+                return response.Response(serializer.aluno_to_json(aluno), status.HTTP_201_CREATED)
+            else: 
+                return response.Response({}, status.HTTP_400_BAD_REQUEST)
+        except:
             return response.Response({}, status.HTTP_400_BAD_REQUEST)
+    
 
     # def get(self, request, **kwargs):
 
@@ -36,12 +41,12 @@ class LoginView(views.APIView):
         if data['tipo_usuario'] == 'professor':
             try:
                 professor = Professor.objects.get(email=data['email'], senha=data['senha'])
-                return response.Response(serializer.professor_to_json(professor), status.HTTP_201_CREATED)
+                return response.Response(serializer.professor_to_json(professor), status.HTTP_200_OK)
             except: 
                 return response.Response({}, status.HTTP_400_BAD_REQUEST)
         elif data['tipo_usuario'] == 'aluno':
             try:
                 aluno = Aluno.objects.get(email=data['email'], senha=data['senha'])
-                return response.Response(serializer.aluno_to_json(aluno), status.HTTP_201_CREATED)
+                return response.Response(serializer.aluno_to_json(aluno), status.HTTP_200_OK)
             except: 
                 return response.Response({}, status.HTTP_400_BAD_REQUEST)
